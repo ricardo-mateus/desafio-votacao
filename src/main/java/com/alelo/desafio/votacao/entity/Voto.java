2f10e1c110fc6c44a7fb6c00c071d2744198d91d
Ricardo Mateus do Nascimento <ricardo.nascimento@dbserver.com.br>
2025-08-11 16:51:40 -0300

    tirada de comentários

--- src/main/java/com/alelo/desafio/votacao/entity/Voto.java
+++ src/main/java/com/alelo/desafio/votacao/entity/Voto.java
@@ -11,16 +11,11 @@ import java.time.LocalDateTime;
 public class Voto {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     @Column(name = "pauta_id", nullable = false)
     private Long pautaId;
-
     @Column(name = "associado_id", nullable = false)
-    private String associadoId; // id único do associado (string para flexibilidade)
-
-    private String cpf; // opcional, para validar via client fake
-
-    private Boolean votoSim; // true = sim, false = nao
-
+    private String associadoId;
+    private String cpf;
+    private Boolean votoSim;
     private LocalDateTime momento;
 }
