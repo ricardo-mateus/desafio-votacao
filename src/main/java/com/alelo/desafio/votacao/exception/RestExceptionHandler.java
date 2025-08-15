84561d9fbb84525b3b1b80e080342b8190a7d295
Ricardo Mateus do Nascimento <ricardo.nascimento@dbserver.com.br>
2025-08-15 13:46:47 -0300

    Camada de controle de exceções

---
+++ src/main/java/com/alelo/desafio/votacao/exception/RestExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.alelo.desafio.votacao.exception;
+
+import com.alelo.desafio.votacao.service.CpfValidationClient;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+
+@ControllerAdvice
+public class RestExceptionHandler {
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity<String> handleBusiness(BusinessException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(CpfValidationClient.CpfNotFoundException.class)
+    public ResponseEntity<String> handleCpfNotFound(CpfValidationClient.CpfNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> fallback(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
+    }
+}
